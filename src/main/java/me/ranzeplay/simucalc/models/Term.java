aec7bf847dad26b1e9329bf280e1e315892e8db6
Jeb Feng <ranzeplay@outlook.com>
2020-10-04 23:16:12 +0800

    Fully migrated to Maven

--- src/main/java/me/ranzeplay/simucalc/models/Term.java
+++ src/main/java/me/ranzeplay/simucalc/models/Term.java
@@ -1,40 +1,36 @@
 package me.ranzeplay.simucalc.models;
 
 public class Term {
-    private String raw;
-    private char operator;
-    private String number;
+	private char operator;
+	private String number;
 
-    public Term(String raw) {
-        this.raw = raw;
+	public Term(String raw) {
+		if (!raw.contains(".")) {
+			raw = raw + ".0";
+		}
 
-        this.operator = raw.charAt(0);
-        this.number = raw.substring(1);
-    }
+		this.operator = raw.charAt(0);
+		this.number = raw.substring(1);
+	}
 
-    public char getOperator() {
-        return operator;
-    }
+	public char getOperator() {
+		return operator;
+	}
 
-    public void setOperator(char operator) {
-        this.operator = operator;
-    }
+	public void setOperator(char operator) {
+		this.operator = operator;
+	}
 
-    public String getNumber() {
-        return number;
-    }
+	public String getNumber() {
+		return number;
+	}
 
-    public void setNumber(String number) {
-        this.number = number;
-    }
+	public void setNumber(String number) {
+		this.number = number;
+	}
 
-    public String getRaw() {
-        return raw;
-    }
-
-    @Override
-    public String toString(){
-        raw = operator + number;
-        return raw;
-    }
+	@Override
+	public String toString() {
+		return operator + number;
+	}
 }
