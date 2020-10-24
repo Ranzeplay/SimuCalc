2961299243abdcfc4b235f30af363bcc604fe8de
Ranzeplay <ranzeplay@outlook.com>
2020-10-24 14:57:24 +0800

    Remove "public" identifier in Tests

--- src/test/java/me/ranzeplay/simucalc/TestCalculation.java
+++ src/test/java/me/ranzeplay/simucalc/TestCalculation.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestCalculation {
 	@Test
-	public void TestLevelOneCalculation() {
+	void TestLevelOneCalculation() {
 		assertEquals(new Term("+2020.0").toString(), LevelOne.Add(new Term("+2000"), new Term("+20")).toString());
 		assertEquals(new Term("+2020.0").toString(), LevelOne.Add(new Term("+2040"), new Term("-20")).toString());
 		assertEquals(new Term("-2020.0").toString(), LevelOne.Add(new Term("-2000"), new Term("-20")).toString());
@@ -21,7 +21,7 @@ public class TestCalculation {
 	}
 
 	@Test
-	public void TestLevelTwoCalculation() {
+	void TestLevelTwoCalculation() {
 		assertEquals("+6.0", LevelTwo.Times(new Term("+3"), new Term("+2")).toString());
 		assertEquals("+81.0", LevelTwo.Times(new Term("+27"), new Term("+3")).toString());
 
@@ -30,7 +30,7 @@ public class TestCalculation {
 	}
 
 	@Test
-	public void TestLevelThreeCalculation() {
+	void TestLevelThreeCalculation() {
 		assertEquals("+4.0", LevelThree.Power(new Term("+2"), new Term("+2")).toString());
 		assertEquals("+1024.0", LevelThree.Power(new Term("+2"), new Term("+10")).toString());
 		assertEquals("-8.0", LevelThree.Power(new Term("-2"), new Term("+3")).toString());
--- src/test/java/me/ranzeplay/simucalc/TestNumbers.java
+++ src/test/java/me/ranzeplay/simucalc/TestNumbers.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestNumbers {
 	@Test
-	public void TestAlignDecimalPoint() {
+	void TestAlignDecimalPoint() {
 		var alignResult = Numbers.AlignDecimalPoint("0.02", "10.247");
 
 		assertEquals("000.0200", alignResult[0]);
@@ -15,21 +15,21 @@ public class TestNumbers {
 	}
 
 	@Test
-	public void TestCompareNumberAbs() {
+	void TestCompareNumberAbs() {
 		assertEquals(0, Numbers.CompareNumberAbs("2.5", "2.50"));
 		assertEquals(1, Numbers.CompareNumberAbs("30.5", "2.50"));
 		assertEquals(-1, Numbers.CompareNumberAbs("0.002", "70.0"));
 	}
 
 	@Test
-	public void TestMoveDecimalPoint() {
+	void TestMoveDecimalPoint() {
 		assertEquals("34.5678", Numbers.MoveDecimalPoint("345.678", -1));
 		assertEquals("34567800.00000000000", Numbers.MoveDecimalPoint("345.678", 5));
 		assertEquals("0.00345678", Numbers.MoveDecimalPoint("345.678", -5));
 	}
 
 	@Test
-	public void TestCleanUselessZeros() {
+	void TestCleanUselessZeros() {
 		assertEquals("0.0", Numbers.CleanUselessZeros("00000.000000000000"));
 		assertEquals("432.321", Numbers.CleanUselessZeros("00432.321000"));
 		assertEquals("4320.00321", Numbers.CleanUselessZeros("004320.00321000"));
